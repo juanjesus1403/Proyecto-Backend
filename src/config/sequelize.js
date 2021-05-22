fe4154e322b299a9f4a0cf6ad1be7229a6639dcc
Juan Jesus Enciso <juanjesus_eq96@hotmail.com>
2021-05-22 18:02:57 -0500

    actualizacion

--- src/config/sequelize.js
+++ src/config/sequelize.js
@@ -1,19 +1,29 @@
 import {Sequelize} from 'sequelize';
 
-export const conexion = new Sequelize(
-  "proyectobackend",
-  "root",//username
-  "mollendo2021",//password
-  {
+export const conexion = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL, {
       dialect: "mysql",
-      host: "127.0.0.1",
-      port:3306,
-      timezone: "-05:00", //no funciona en SQLITE
-      dialectOptions:{
+      timezone: "-05:00",
+      logging: false,
+      dialectOptions: {
+        dateStrings: true,
+      },
+    })
+
+  :new Sequelize(
+      "proyectobackend",
+      "root",//username
+      "root",//password
+  {
+        dialect: "mysql",
+        host: "127.0.0.1",
+        port:3306,
+        timezone: "-05:00", //no funciona en SQLITE
+        dialectOptions:{
         //sirve para que al momento de mostrar las fechas, automaticamente las convierta en string y no tener que hacer una conversacion manual
         dateStrings:true,
       },
 
-      logging: false,
+        logging: false,
   }
 );
