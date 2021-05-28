cd78d8da527c307af1c066a4f094f95420fc6d05
Juan Jesus Enciso <juanjesus_eq96@hotmail.com>
2021-05-27 21:56:13 -0500

    agregar router

--- src/routes/categoria.js
+++ src/routes/categoria.js
@@ -2,7 +2,5 @@ import {Router} from "express";
 import * as categoria_controller from "../controllers/categoria";
 
 export const categoria_router = Router()
-categoria_router
-    .route("/categoria")
-    .post(categoria_controller.crearCategoria)
-    .get(categoria_controller.listarCategoria);
+categoria_router.route("/crearcategoria").post(categoria_controller.crearCategoria);
+categoria_router.route("/categoria").get(categoria_controller.listarCategoria);
--- src/routes/locales.js
+++ src/routes/locales.js
@@ -3,9 +3,9 @@ import * as locales_controller from "../controllers/locales";
 import locales from "../models/locales";
 
 export const locales_router = Router()
-locales_router.route("/locales").post(locales_controller.crearLocales);
-locales_router.route("/localesobtener").get(locales_controller.listarLocales);
+locales_router.route("/crearlocales").post(locales_controller.crearLocales);
+locales_router.route("/locales").get(locales_controller.listarLocales);
 
-locales_router.route("/localesagregar/:id").put(locales_controller.actualizarLocales);
+locales_router.route("/locales/:id").put(locales_controller.actualizarLocales);
 
 locales_router.route("/localeseliminar/:id").delete(locales_controller.eliminarLocales);
